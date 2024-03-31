b0e48c28da0955dc69ac867b2baefad385bf3942
Gemma Emery <gemmaeme@student.21-school.ru>
2024-03-31 14:37:08 +0700

    fix

--- Manager Server/src/main/kotlin/com/example/models/RegisteredUser.kt
+++ Manager Server/src/main/kotlin/com/example/models/RegisteredUser.kt
@@ -1,7 +1,9 @@
 package com.example.models
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class RegisteredUser(
     @SerialName("user_id") val userId: Int,
     val role: UserRole
